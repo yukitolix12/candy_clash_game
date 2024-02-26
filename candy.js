ffc81ba95c96f8f1531003a274b08fee656c4b5e
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-02-26 20:25:17 +0900

    create startGame function to assign img tag and id

--- candy.js
+++ candy.js
@@ -1,10 +1,22 @@
 
 var candies = ["Blue", "Orange", "Green","Yellow", "Red", "Purple"];
 var board = [];
-var row = 9;
+var rows = 9;
 var column = 9;
 var score = 0;
 
 window.onload = function(){
     startGame();
+}
+
+function startGame {
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c + column; c++){
+            //img
+            let tile = document.createElement("img");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.src = "./images" + randomCandy() + ".png";
+        }
+    }
 }
