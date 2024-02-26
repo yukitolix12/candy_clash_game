d04b5ca934b39503592e4ac54dc5e642f6509828
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-02-26 20:32:33 +0900

    fix typo

--- candy.js
+++ candy.js
@@ -9,14 +9,14 @@ window.onload = function(){
     startGame();
 }
 
-function startGame {
+function startGame() {
     for (let r = 0; r < rows; r++) {
         let row = [];
         for (let c = 0; c + column; c++){
             //img
             let tile = document.createElement("img");
             tile.id = r.toString() + "-" + c.toString();
-            tile.src = "./images" + randomCandy() + ".png";
+            tile.src = "./images/" + randomCandy() + ".png"
         }
     }
 }
