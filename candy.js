37a4b6a85c18d428e83517c01892774bb58a50f3
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-03-01 18:17:51 +0900

    before creating codes in if sentence,put formula on candies

--- candy.js
+++ candy.js
@@ -103,6 +103,7 @@ function crushThree() {
             let candy1 = board[r][c];
             let candy2 = board[r][c+1];
             let candy3 = board[r][c+2];
+            if(candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blans"))
         }
     }
 }
