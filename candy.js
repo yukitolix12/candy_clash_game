404be36eb9a7dfeee9d055711bdf37aa0d1cdc43
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-03-03 08:51:06 +0900

    remove candy1,2,3.src,then put returnto combine rows

--- candy.js
+++ candy.js
@@ -139,9 +139,7 @@ function checkValid() {
             let candy2 = board[r][c+1];
             let candy3 = board[r][c+2];
             if(candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")){
-                candy1.src = "./images/blank.png";
-                candy2.src = "./images/blank.png";
-                candy3.src = "./images/blank.png";
+                return true;
             }
         }
     }
@@ -153,9 +151,7 @@ function checkValid() {
             let candy2 = board[r+1][c];
             let candy3 = board[r+2][c];
             if(candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")){
-                candy1.src = "./images/blank.png";
-                candy2.src = "./images/blank.png";
-                candy3.src = "./images/blank.png";
+                return true;
             }
         }
     }
