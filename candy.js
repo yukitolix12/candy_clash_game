562744a421873de177b8ad27e885b73ea1bb44ca
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-03-04 19:39:02 +0900

    board connects to images and randomCandy

--- candy.js
+++ candy.js
@@ -192,7 +192,7 @@ function slideCandy() {
 function generateCandy(){
     for(let c = 0; c < columns; c++){
        if(board[0][c].src.includes["blanks"]){
-        
+          board[0][c].src = "./images/" + randomCandy() + ".png";
        } 
     }
 }
