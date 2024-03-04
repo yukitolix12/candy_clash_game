9e61e456cfe8011f76a6663531ba82f331b1cca3
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-03-04 19:42:32 +0900

    fix typo that can run game on CandyCrush

--- candy.js
+++ candy.js
@@ -191,7 +191,7 @@ function slideCandy() {
 
 function generateCandy(){
     for(let c = 0; c < columns; c++){
-       if(board[0][c].src.includes["blanks"]){
+       if(board[0][c].src.includes("blank")){
           board[0][c].src = "./images/" + randomCandy() + ".png";
        } 
     }
